ba9e8797693ae460b42344e3d687983a5391f2ac
Imran-72 <petrow318@gmail.com>
2022-01-23 13:29:05 +0300

    Каррирование

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const App = () => {
   const double = (number) => number * 2;
   const square = (number) => number * number;
   const half = (number) => number / 2;
-  const mathCalculate = pipe(half, square, double);
+  const devide = (num2) => {
+    return function (num1) {
+      return num1 / num2;
+    };
+  };
+  const mathCalculate = pipe(half, square, double, devide(3));
   // const mathCalculate = pipe(double, square, half);
   return <h1>{mathCalculate(x)}</h1>;
 };
