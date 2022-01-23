1d92b63e65363e23ff094f1ef9b9fc7f1b6ae723
Imran-72 <petrow318@gmail.com>
2022-01-23 16:45:07 +0300

    Разработка redux с нуля. Dispatch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function createStore(initialState) {
+  let state = initialState;
+
+  function dispatch(action) {
+    if (action.type === "test") {
+      let newArray = [...state];
+      let elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex].completed = true;
+      state = newArray;
+    }
+    console.log(state);
+  }
+  function getState() {
+    return state;
+  }
+
+  return {
+    getState,
+    dispatch,
+  };
+}
+
+const store = createStore([{ id: 1, completed: false }]);
+
 const App = () => {
-  return <h1>App</h1>;
+  console.log(store.getState());
+  return (
+    <>
+      <h1>App</h1>
+      <button
+        onClick={() => store.dispatch({ type: "test", payload: { id: 1 } })}
+      >
+        click
+      </button>
+    </>
+  );
 };
 
 ReactDOM.render(
