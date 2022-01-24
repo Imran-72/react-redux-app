95c95fbbecfe9577ae1614a2e9c05c7d03c8714e
Imran-72 <petrow318@gmail.com>
2022-01-24 14:28:54 +0300

    разработка redux с нуля. Display Tasks

--- src/index.js
+++ src/index.js
@@ -32,18 +32,30 @@ function createStore(reducer, initialState) {
   };
 }
 
-const store = createStore(testReducer, [{ id: 1, completed: false }]);
-console.log(store.getState());
+const store = createStore(testReducer, [
+  { id: 1, description: "Task 1", completed: false },
+  { id: 2, description: "Task 2", completed: false },
+]);
 
 const App = () => {
-  const compliteTask = () => {
-    store.dispatch({ type: "test", payload: { id: 1 } });
+  const state = store.getState();
+  const compliteTask = (taskId) => {
+    store.dispatch({ type: "test", payload: { id: taskId } });
     console.log(store.getState());
   };
   return (
     <>
       <h1>App</h1>
-      <button onClick={compliteTask}>click</button>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.description}</p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => compliteTask(el.id)}>Completed</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
